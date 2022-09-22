0dc13c66a89230aaec1cd0e0658745d7dc021b30
Jose <josealejandrogilmendez08@gmail.com>
2022-09-22 19:11:45 -0400

    Complete 1-3 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removed) {
+    for (const iterator of array) {
+        if (iterator === removed) {
+            array.splice(array.indexOf(removed), array.indexOf(removed)+1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
