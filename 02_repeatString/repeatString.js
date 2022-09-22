2dbc36b6715ce6aa1bd0dca984514512df1897b6
Jose <josealejandrogilmendez08@gmail.com>
2022-09-22 19:12:22 -0400

    complete 1-3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, iteration) {
+    if (iteration < 0) return 'ERROR';
+    
+    let stringRepeated = '';
+    for (let i = 0; i < iteration; i++) {
+        stringRepeated += string;
+    }
+    return stringRepeated;
 };
 
 // Do not edit below this line
